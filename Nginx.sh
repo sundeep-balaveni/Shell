f57e5f90236a64ad5f54fcc01a5a45fe6085f06d
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-01-31 16:40:12 +0530

    33

--- Nginx.sh
+++ Nginx.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo su -
+USERID=$(id -u)
 
-dnf install nginx -y
+if [ USERID -nq 0 ]; then 
 
+    echo ("run this script as sudo access")
+
+fi 
+
+echo "installing ngjnx"
+
+dnf install nginx -y 
