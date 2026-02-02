be8cfca4c9fcf3584972aaad089f6df659db8e18
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-02-02 21:18:13 +0530

    roboshop

--- Nginx.sh
+++ Nginx.sh
@@ -6,7 +6,7 @@ if [ "$USERID" -ne 0 ]; then
     echo "Please run this script with sudo access"
     exit 1
 fi
-
+        
 echo "Installing nginx"
 dnf install nginx -y
 
---
+++ Roboshop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+REGION="us-east-1"
+AMI_ID="ami-0220d79f3f480ecf5"     # Amazon Linux 2
+INSTANCE_TYPE="t2.micro"
+SECURITY_GROUP_ID="sg-0d6a680fc44091364"
+SUBNET_ID="subnet-0a79d446f5450259c"
+TAG_NAME="ShellCreatedEC2"
