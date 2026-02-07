79af99e292be57b38b63cb52d71c3bebb0c26990
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-02-07 23:29:31 +0530

    sdgs

--- Roboshop/Mongodb.sh
+++ Roboshop/Mongodb.sh
@@ -2,12 +2,16 @@
 
 USER_ID=$(id -u)
 
+
+LOGS_FOLDER="/var/log/roboshop"
+LOG_FILE="$LOGS_FOLDER/mongo.log"
+
 mkdir -p $LOGS_FOLDER
 
 
 if [ $USER_ID -eq 0 ]; then
 
-cp Roboshop/Mongo.repo  /etc/yum.repos.d/mongo.repo 
+cp Mongo.repo  /etc/yum.repos.d/mongo.repo 
 
 dnf install mongodb-org -y 
 
@@ -21,17 +25,20 @@ systemctl restart mongod
 
 
 
-if [$? -eq 0 ]; then
+if [ $? -eq 0 ]; then
 
 echo "Mongo is installed succesfully "
 
 else 
 
-echo "insatlling failed"  | tee -a  $LOGS_FOLDER
+echo "insatlling failed"  | tee -a  $LOG_FILE
 exit 1 
 
 fi
 
+else
+
+echo "Please run As root-user"
 
 
 fi 
