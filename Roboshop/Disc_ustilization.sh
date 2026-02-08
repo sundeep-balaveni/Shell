c35fb46a864e513bae4f53df3e7faee35baf8326
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-02-08 22:41:38 +0530

    fag

--- Roboshop/Disc_ustilization.sh
+++ Roboshop/Disc_ustilization.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 
-THRESHOLD=80
+$THRESHOLD=80
 
 
 DISK_UTILIZATION=$(df -h / | grep / | awk '{print $5}' | sed 's/%//g')
 
-if [DISK_UTILIZATION -gt THRESHOLD] ; then
+if [ $DISK_UTILIZATION -gt $THRESHOLD ] ; then
 
   echo "Disk is more than 80 percent"
 
 else
-
 echo "Less than 80 %"
-
 fi
 
 echo "current disk usage : $DISK_UTILIZATION"
